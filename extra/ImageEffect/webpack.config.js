2e4669b070a424a5ce6abaf0c1bcdf673c63555a
foram140 <forampatel@appsly.in>
2023-01-25 15:17:42 +0530

    rustAssembly

---
+++ extra/ImageEffect/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require ('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const wasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
+module.exports ={
+    entry : './public/main.js',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    plugins:[
+        new HTMLWebpackPlugin ({
+            template: './public/index.html'
+        }),
+        new wasmPackPlugin({
+            createDirectory: path.resolve(__dirname,'.' ),
+        })
+    ],
+    experiments:{
+        asyncwebAssembly:true
+    }
+}
